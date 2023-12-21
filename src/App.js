ee9e235aebeda9ed20a4e28132b00a80f7930b3f
Patrick Oser <patrick.oser148@gmail.com>
2023-12-21 15:10:28 -0500

    Import and add blog components to App.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import PostPage from './components/PostPage';
+import Home from './components/Home';
+import Nav from './components/Nav';
+import Header from './components/Header';
+import About from './components/About';
+import EditPage from './components/EditPage';
+import Footer from './components/Footer';
+import Missing from './components/Missing';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>App</h1>
-      </header>
+      <Header />
+      <Nav />
+      <Home />
+      <PostPage />
+      <EditPage />
+      <About />
+      <Missing />
+      <Footer />
     </div>
   );
 }
