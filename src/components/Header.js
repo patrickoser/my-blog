5abc9f602c6eda39bb783eb9a77cdca73d2e656a
Patrick Oser <patrick.oser148@gmail.com>
2023-12-28 16:05:40 -0500

    Add nav bar to Nav.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom"
+
 const Header = () => {
     return (
         <div className="Header">
-            <h2>Header</h2>
+            <Link to={'/'}>
+                <h2>Header</h2>
+            </Link>
         </div>
     )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom"
+
 const Nav = () => {
     return (
         <div className="Nav">
-            <h2>Nav</h2>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/postpage">Post</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                </ul>
+            </nav>
         </div>
     )
 }
