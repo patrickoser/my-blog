1ff0b17e4a2073a51b8b8d8b61741aed685eeb87
Patrick Oser <patrick.oser148@gmail.com>
2024-01-04 12:51:43 -0500

    Delete.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,7 @@
 import axios from "../api/axios"
 
 const Home = () => {
-    const getPosts = async () => {
-        try {
-            const response = await axios.get('')
-        } catch (err) {
-            console.log(err)
-        }
-    }
+
 
     return (
         <div className="Home">
