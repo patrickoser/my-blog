f21e4634b35d8b0073b245e2b92ca040b0163fb4
Patrick Oser <patrick.oser148@gmail.com>
2024-01-05 14:32:50 -0500

    Define post/setPost using useState.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NewPost from './components/NewPost';
 import Missing from './components/Missing';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/Layout';
+import { useState } from 'react';
 
 // createBrowserRouter uses Layout as the root element that is rendered first.
 // All of the children nested within Layout are then designated their own path
@@ -45,6 +46,8 @@ const router = createBrowserRouter([
 
 // App() returns router which contains all of the layout/routes for the site
 function App() {
+  const [post, setPost] = useState([]);
+  
   return (
     <div className="App">
       <RouterProvider router={router} />;
