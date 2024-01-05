86e43283cbe58d62d2d84728dc22a14668375a63
Patrick Oser <patrick.oser148@gmail.com>
2024-01-05 14:35:02 -0500

    Define postTitle/setPostTitle using useState.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import NewPost from './components/NewPost';
 import Missing from './components/Missing';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/Layout';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 // createBrowserRouter uses Layout as the root element that is rendered first.
 // All of the children nested within Layout are then designated their own path
@@ -47,7 +47,8 @@ const router = createBrowserRouter([
 // App() returns router which contains all of the layout/routes for the site
 function App() {
   const [post, setPost] = useState([]);
-  
+  const [postTitle, setPostTitle] = useState('');
+
   return (
     <div className="App">
       <RouterProvider router={router} />;
