7d9e782ce48a0f61b62f2966851d8d695e009ed9
Patrick Oser <patrick.oser148@gmail.com>
2024-01-05 14:10:42 -0500

    Add comments to App.js.

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import About from './components/About';
 import EditPage from './components/EditPage';
 import NewPost from './components/NewPost';
 import Missing from './components/Missing';
-import { createBrowserRouter, Link, Route, RouterProvider, Routes } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/Layout';
 
+// createBrowserRouter uses Layout as the root element that is rendered first.
+// All of the children nested within Layout are then designated their own path
 const router = createBrowserRouter([
   {
     element: <Layout />,
@@ -41,8 +43,7 @@ const router = createBrowserRouter([
   { path: "*", element: App },
 ]);
 
-
-
+// App() returns router which contains all of the layout/routes for the site
 function App() {
   return (
     <div className="App">
