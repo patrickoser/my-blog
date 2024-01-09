187ea3cfd498617a9f283609b5fb3ed124403d4f
Patrick Oser <patrick.oser148@gmail.com>
2024-01-09 15:16:55 -0500

    Add comments.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,6 +4,9 @@ import Footer from './Footer';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
+
+    // Returns the Header, Nav, and Footer with the Outlet nested within that contains the
+    // routes.
     return (
         <>
             <Header />
