b15db9f57965fcf084bf2f5d5a6a2d4d07bb4561
Patrick Oser <patrick.oser148@gmail.com>
2024-01-14 12:24:04 -0500

    Import components needed in Router.js.

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,4 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Home";
+import PostPage from "./PostPage";
+import About from "./About";
+import EditPage from "./EditPage";
+import NewPost from "./NewPost";
+import Missing from "./Missing";
+import Layout from "./Layout";
+import App from "../App";
 
 // createBrowserRouter uses Layout as the root element that is rendered first.
 // All of the children nested within Layout are then designated their own path
@@ -35,6 +43,8 @@ const Router = () => {
         },
         { path: "*", element: App },
     ]);
+
+    return 
 }
 
 export default Router
