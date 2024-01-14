2cefa3463abeec27f4888cbf605ac509439386fb
Patrick Oser <patrick.oser148@gmail.com>
2024-01-14 12:20:08 -0500

    Reformat Router.js.

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,37 +1,37 @@
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // createBrowserRouter uses Layout as the root element that is rendered first.
 // All of the children nested within Layout are then designated their own path
 const Router = () => {
     const router = createBrowserRouter([
         {
-        element: <Layout />,
-        children: [
-            {
-            path: "/",
-            element: <Home />,
-            },
-            {
-            path: "/postpage",
-            element: <PostPage />,
-            },
-            {
-            path: "/about",
-            element: <About />,
-            },
-            {
-            path: "/editpage",
-            element: <EditPage />,
-            },
-            {
-            path: "/newpost",
-            element: <NewPost />,
-            },
-            {
-            path: "*",
-            element: <Missing />,
-            },
-        ],
+            element: <Layout />,
+            children: [
+                {
+                    path: "/",
+                    element: <Home />,
+                },
+                {
+                    path: "/postpage",
+                    element: <PostPage />,
+                },
+                {
+                    path: "/about",
+                    element: <About />,
+                },
+                {
+                    path: "/editpage",
+                    element: <EditPage />,
+                },
+                {
+                    path: "/newpost",
+                    element: <NewPost />,
+                },
+                {
+                    path: "*",
+                    element: <Missing />,
+                },
+            ],
         },
         { path: "*", element: App },
     ]);
