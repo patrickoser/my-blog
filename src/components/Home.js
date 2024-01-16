a0b663d1ddd88d67a4e3e59f347e0dbd15150aaa
Patrick Oser <patrick.oser148@gmail.com>
2024-01-16 08:36:45 -0500

    Comments added to Home.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,10 @@
 import Feed from "./Feed"
 
+// 'posts' is passed as a deconstructed object to Home.
+// Home then takes the contents of 'posts', uses a ternary operator to check their length, and
+// if that length is >= 1 then the Feed component will be called, with posts as a prop, and map
+// all available posts to the page.
+// If the boolean comes back as false, or < 1, then 'No posts to display' will be displayed.
 const Home = ({ posts }) => {
     return (
         <div className="Home">
