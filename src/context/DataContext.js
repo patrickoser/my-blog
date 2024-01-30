b6d1e93ea4761d53690adda4b207a63c937e9f8a
Patrick Oser <patrick.oser148@gmail.com>
2024-01-30 15:11:10 -0500

    Define DataContext and DataProvider in DataContext.js.

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -2,3 +2,14 @@ import { createContext } from "react";
 
 const DataContext = createContext({})
 
+export const DataProvider = ({ children }) => {
+    return (
+        <DataContext.Provider value={{
+
+        }}>
+            {children}
+        </DataContext.Provider>
+    )
+}
+
+export default DataContext
