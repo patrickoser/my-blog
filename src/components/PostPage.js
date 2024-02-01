c39fd4835f84bd85ab29a2d5b994361aa5978b85
Patrick Oser <patrick.oser148@gmail.com>
2024-02-01 18:17:38 -0500

    Declare id using useParams.

--- src/components/PostPage.js
+++ src/components/PostPage.js
@@ -1,8 +1,10 @@
 import { useContext } from "react"
 import DataContext from '../context/DataContext'
+import { useParams } from "react-router-dom"
 
 const PostPage = () => {
     const { posts } = useContext(DataContext)
+    const { id } = useParams()
     return (
         <div className="PostPage">
             
