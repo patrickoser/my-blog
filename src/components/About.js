cf39f1c06831bd593c8bfa4a0d6d806a47733977
Patrick Oser <patrick.oser148@gmail.com>
2024-02-02 14:07:53 -0500

    Add some content to About and add Contact.js.

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,13 @@
 const About = () => {
     return (
-        <div className="About">
-            <h1>About.</h1>
-        </div>
+        <main className="About">
+            <h1>Welcome to my blog!</h1>
+            <p>
+                This is a place where I will write about my journey as I learn how to code.
+                Different problems that I encounter, the solutions that I found, and my general
+                thoughts on all things programming.
+            </p>
+        </main>
     )
 }
 
