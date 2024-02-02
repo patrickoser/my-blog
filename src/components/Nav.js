c2f9c569d792c2ae172bfe9b2366ee93f064fe87
Patrick Oser <patrick.oser148@gmail.com>
2024-02-02 14:10:42 -0500

    Add Contact link to Nav.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -28,6 +28,9 @@ const Nav = () => {
                     <li>
                         <Link to="/about">About</Link>
                     </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
                 </ul>
             </nav>
         </div>
