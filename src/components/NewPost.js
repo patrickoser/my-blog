884fca869789c5901623c0fa159610de1de5b48b
Patrick Oser <patrick.oser148@gmail.com>
2024-02-04 15:24:14 -0500

    Declare context variables needed for NewPost form.

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,11 +1,16 @@
+import { useContext } from "react"
+import DataContext from "../context/DataContext"
+
 const NewPost = () => {
+
+    const { createPost, postTitle, postBody, setPostTitle, setPostBody } = useContext(DataContext)
     return (
         <div className="NewPost">
             <form onSubmit={createPost}method="post">
                 <label htmlFor="postTitle">Title</label>
-                <input type="text" id="postTitle" name="postTitle" />
+                <input type="text" id="postTitle" value={postTitle} name="postTitle" onChange={setPostTitle} required />
                 <label htmlFor="postBody">Body</label>
-                <input type="text" id="postBody" name="postBody" />
+                <input type="text" id="postBody" value={postBody} name="postBody" onChange={setPostBody} required />
                 <button type="submit"></button>
             </form>
         </div>
