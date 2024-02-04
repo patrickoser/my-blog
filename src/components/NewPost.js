7b06b5fa0c227313a78bda1d4f855c504b1a2d6c
Patrick Oser <patrick.oser148@gmail.com>
2024-02-04 14:38:55 -0500

    Create form in NewPost.

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,7 +1,13 @@
 const NewPost = () => {
     return (
         <div className="NewPost">
-            <h1>New Post</h1>
+            <form onSubmit={createPost}method="post">
+                <label htmlFor="postTitle">Title</label>
+                <input type="text" id="postTitle" name="postTitle" />
+                <label htmlFor="postBody">Body</label>
+                <input type="text" id="postBody" name="postBody" />
+                <button type="submit"></button>
+            </form>
         </div>
     )
 }
