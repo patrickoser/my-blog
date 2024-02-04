a628192c8ad738b64c89b674dd9e55ab68701d39
Patrick Oser <patrick.oser148@gmail.com>
2024-02-04 15:28:51 -0500

    Format NewPost form to be more readable.

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -8,9 +8,23 @@ const NewPost = () => {
         <div className="NewPost">
             <form onSubmit={createPost}method="post">
                 <label htmlFor="postTitle">Title</label>
-                <input type="text" id="postTitle" value={postTitle} name="postTitle" onChange={setPostTitle} required />
+                <input 
+                    type="text" 
+                    id="postTitle" 
+                    value={postTitle} 
+                    name="postTitle" 
+                    onChange={(e) => setPostTitle(e.target.value)} 
+                    required 
+                />
                 <label htmlFor="postBody">Body</label>
-                <input type="text" id="postBody" value={postBody} name="postBody" onChange={setPostBody} required />
+                <input 
+                    type="text" 
+                    id="postBody" 
+                    value={postBody} 
+                    name="postBody" 
+                    onChange={(e) => setPostBody(e.target.value)} 
+                    required 
+                />
                 <button type="submit"></button>
             </form>
         </div>
