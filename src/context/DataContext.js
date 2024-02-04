5a4f452807ddf2fafc1e58b8af45adfa8132338a
Patrick Oser <patrick.oser148@gmail.com>
2024-02-04 13:02:43 -0500

    Add createPost function to post data to the server.

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -13,7 +13,20 @@ export const DataProvider = ({ children }) => {
     const [postBody, setPostBody] = useState('')
 
     const createPost = async (e) => {
-
+        e.preventDefault()
+        const id = posts.length ? posts[posts.length - 1].id + 1 : 1
+        const datetime = format(new Date(), 'MMMM dd yyyy pp')
+        const newPost = { id, title: postTitle, datetime, body: postBody }
+        try {
+            const response = await api.post('/posts', newPost)
+            const allPosts = [...posts, response.data]
+            setPosts(allPosts)
+            setPostTitle('')
+            setPostBody('')
+            navigate('/')
+        } catch(err) {
+            console.log(`Error: ${err}`)
+        }
     }
 
     // Pulls posts in from server and updates state to display them when page loads
