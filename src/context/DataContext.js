e4bb0f9fcbe4b6e55790d8df915ce2edc3c35df7
Patrick Oser <patrick.oser148@gmail.com>
2024-02-04 13:15:04 -0500

    Import format for timestamps on posts.

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import api from '../api/postsAxios'
 import { useNavigate } from "react-router-dom";
+import { format } from 'date-fns'
 
 const DataContext = createContext({})
 
