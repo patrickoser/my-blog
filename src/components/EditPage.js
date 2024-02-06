cea61193e7dda80a901fb3d3d096852c1b64c97c
Patrick Oser <patrick.oser148@gmail.com>
2024-02-06 14:02:05 -0500

    Set up form in EditPage.

--- src/components/EditPage.js
+++ src/components/EditPage.js
@@ -1,8 +1,28 @@
 const EditPage = () => {
     return (
-        <div className="EditPage">
-            <h1>Edit Page</h1>
-        </div>
+        <main className="NewPost">
+            <form className="newPostForm" onSubmit={createPost}>
+                <label htmlFor="postTitle">Title</label>
+                <input 
+                    type="text" 
+                    id="postTitle" 
+                    value={postTitle} 
+                    name="postTitle" 
+                    onChange={(e) => setPostTitle(e.target.value)} 
+                    required 
+                />
+                <label htmlFor="postBody">Body</label>
+                <textarea 
+                    type="text" 
+                    id="postBody" 
+                    value={postBody} 
+                    name="postBody" 
+                    onChange={(e) => setPostBody(e.target.value)} 
+                    required 
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </main>
     )
 }
 
