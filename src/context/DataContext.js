26935181c38d2cf45a9252d8c399c7e3a09022a2
Patrick Oser <patrick.oser148@gmail.com>
2024-02-06 14:27:11 -0500

    Clean up unused imports.

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import api from '../api/postsAxios'
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { format } from 'date-fns'
 
 const DataContext = createContext({})
