42d1ebf767e5d9c6cb1df92ed5abf46979021e34
Patrick Oser <patrick.oser148@gmail.com>
2024-02-13 12:45:14 -0500

    Add required property to input fields.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,18 +9,20 @@ const Contact = () => {
             <p><b>Email: </b>patrick.oser1@gmail.com</p>
             <p><b>Github: </b><a href="https://github.com/patrickoser">https://github.com/patrickoser</a></p>
             <p><b>LinkedIn: </b><a href="https://www.linkedin.com/in/patrick-oser-841648260/">https://www.linkedin.com/in/patrick-oser-841648260/</a></p>
-            <form action="" className="contactForm">
-                <label htmlFor="">Name</label>
+            <form action="https://formsubmit.co/your@email.com" className="contactForm">
+                <label>Name</label>
                 <input 
                     type="text"
                     name="name"
                     placeholder="Your name"
+                    required
                 />
                 <label htmlFor="">Email</label>
                 <input 
                     type="text"
                     name="email"
                     placeholder="Your email"
+                    required
                 />
                 <label htmlFor="">Message</label>
                 <textarea 
@@ -30,6 +32,7 @@ const Contact = () => {
                     placeholder="Your message"
                     cols="30" 
                     rows="10" 
+                    required
                 />
             </form>
         </main>
