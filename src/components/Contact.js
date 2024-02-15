4734622e4066282dbd0c838fa55ce40e2fca3c47
Patrick Oser <patrick.oser148@gmail.com>
2024-02-15 10:33:59 -0500

    Small changes.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,39 +2,43 @@ const Contact = () => {
 
     return(
         <main className="ContactPage">
-            <h1>Need to get in touch?</h1>
-            <p><b>Name: </b>Patrick Oser</p>
-            <p><b>Email: </b>patrick.oser1@gmail.com</p>
-            <p><b>Github: </b><a href="https://github.com/patrickoser">https://github.com/patrickoser</a></p>
-            <p><b>LinkedIn: </b><a href="https://www.linkedin.com/in/patrick-oser-841648260/">https://www.linkedin.com/in/patrick-oser-841648260/</a></p>
-            <form target="_blank" action="https://formsubmit.co/0468385d2fc2c9897f3f7936e51b7e3c" method="POST" className="contactForm">
-                <label>Name</label>
-                <input 
-                    type="text"
-                    name="name"
-                    placeholder="Your name"
-                    required
-                />
-                <label>Email</label>
-                <input 
-                    type="text"
-                    name="email"
-                    placeholder="Your email"
-                    required
-                />
-                <label>Message</label>
-                <textarea 
-                    type="textarea"
-                    name="message" 
-                    id="message"
-                    placeholder="Your message"
-                    cols="30" 
-                    rows="10" 
-                    required
-                />
-                <input type="hidden" name="_captcha" value={false} />
-                <button type="submit">Send</button>
-            </form>
+            <section className="contactInfo">
+                <h1>Need to get in touch?</h1>
+                <p><b>Name: </b>Patrick Oser</p>
+                <p><b>Email: </b>patrick.oser1@gmail.com</p>
+                <p><b>Github: </b><a href="https://github.com/patrickoser">https://github.com/patrickoser</a></p>
+                <p><b>LinkedIn: </b><a href="https://www.linkedin.com/in/patrick-oser-841648260/">https://www.linkedin.com/in/patrick-oser-841648260/</a></p>
+            </section>
+            <section>
+                <form target="_blank" action="https://formsubmit.co/0468385d2fc2c9897f3f7936e51b7e3c" method="POST" className="contactForm">
+                    <label>Name</label>
+                    <input 
+                        type="text"
+                        name="name"
+                        placeholder="Your name"
+                        required
+                    />
+                    <label>Email</label>
+                    <input 
+                        type="text"
+                        name="email"
+                        placeholder="Your email"
+                        required
+                    />
+                    <label>Message</label>
+                    <textarea 
+                        type="textarea"
+                        name="message" 
+                        id="message"
+                        placeholder="Your message"
+                        cols="30" 
+                        rows="10" 
+                        required
+                    />
+                    <input type="hidden" name="_captcha" value={false} />
+                    <button type="submit">Send</button>
+                </form>
+            </section>
         </main>
     )
 }
