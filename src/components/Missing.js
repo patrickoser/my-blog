6a67fea5fcdbd77a3bee194c1cf2c50e79da8dad
Patrick Oser <patrick.oser148@gmail.com>
2024-02-16 15:27:00 -0500

    Edit Missing page.

--- src/components/Missing.js
+++ src/components/Missing.js
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom"
+import Home from "./Home"
+
 const Missing = () => {
     return (
-        <main className="container-fluid">
-            <h1>Missing</h1>
+        <main className="container">
+            <hgroup>
+                <h2>Opps... Looks like this page doesn't exists</h2>
+                <h3>Want to return <Link to={'/'}>home</Link>?</h3>
+            </hgroup>
         </main>
     )
 }
