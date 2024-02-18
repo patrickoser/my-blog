d300c694b9ba23e6758d95110dcc95b12d165e70
Patrick Oser <patrick.oser148@gmail.com>
2024-02-18 09:03:28 -0500

    SMall changes to Missing.

--- src/components/Missing.js
+++ src/components/Missing.js
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom"
-import Home from "./Home"
 
 const Missing = () => {
     return (
         <main className="container">
             <hgroup>
-                <h2>Opps... Looks like this page doesn't exists</h2>
+                <h2>Opps... Looks like this page doesn't exists.</h2>
+                <br />
                 <h3>Want to return <Link to={'/'}>home</Link>?</h3>
             </hgroup>
         </main>
