b53d545f6e72f9c7983455cc81d9f1b4db0e8643
Patrick Oser <patrick.oser148@gmail.com>
2024-02-20 14:16:53 -0500

    Format contact form to be responsive.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,8 @@
 const Contact = () => {
 
     return(
-        <main className="container">
-            <section>
+        <main id="contactContainer" className="container">
+            <section className="contactInfo">
                 <article>
                     <h1>Need to get in touch?</h1>
                     <p><b>Name: </b>Patrick Oser</p>
@@ -14,20 +14,26 @@ const Contact = () => {
             <section>
                 <article>
                     <form target="_blank" action="https://formsubmit.co/0468385d2fc2c9897f3f7936e51b7e3c" method="POST" className="contactForm">
-                        <label>Name</label>
-                        <input 
-                            type="text"
-                            name="name"
-                            placeholder="Your name"
-                            required
-                        />
-                        <label>Email</label>
-                        <input 
-                            type="text"
-                            name="email"
-                            placeholder="Your email"
-                            required
-                        />
+                        <div className="contactNameEmailCon">
+                            <div className="nameLabelInput">
+                                <label>Name</label>
+                                <input 
+                                    type="text"
+                                    name="name"
+                                    placeholder="Your name"
+                                    required
+                                />
+                            </div>
+                            <div className="emailLabelInput">
+                                <label>Email</label>
+                                <input 
+                                    type="text"
+                                    name="email"
+                                    placeholder="Your email"
+                                    required
+                                />
+                            </div>
+                        </div>
                         <label>Message</label>
                         <textarea 
                             type="textarea"
