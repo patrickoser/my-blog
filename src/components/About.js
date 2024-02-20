acc3db37c97ee838789d61ee299b80f692460481
Patrick Oser <patrick.oser148@gmail.com>
2024-02-20 13:09:12 -0500

    Wrap About page in article element to create card look.

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,20 @@
 const About = () => {
     return (
         <main className="container">
-            <h1>Welcome to my blog!</h1>
-            <p>
-                This is a place where I will write about my journey as I learn how to code.
-                Different problems that I encounter, the solutions that I found, and my general
-                thoughts on all things programming.
-            </p>
-            <br />
-            <p>
-                If you'd like to get in touch with me then head over to the contact page. There 
-                you will find my email, github, and, if you choose, a form you can fill out 
-                and I will get back to you ASAP.
-            </p>
+            <article>
+                <h1>Welcome to my blog!</h1>
+                <p>
+                    This is a place where I will write about my journey as I learn how to code.
+                    Different problems that I encounter, the solutions that I found, and my general
+                    thoughts on all things programming.
+                </p>
+                <br />
+                <p>
+                    If you'd like to get in touch with me then head over to the contact page. There 
+                    you will find my email, github, and, if you choose, a form you can fill out 
+                    and I will get back to you ASAP.
+                </p>
+            </article>
         </main>
     )
 }
