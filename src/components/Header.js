79057b32a9f0e86764f0ade505a3bb2105602440
Patrick Oser <patrick.oser148@gmail.com>
2024-02-24 13:22:27 -0500

    Add useLocalStorage hook to Header for theme.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,21 @@
 import { Link } from "react-router-dom"
+import useLocalStorage from 'use-local-storage'
 import Nav from "./Nav"
 
 const Header = () => {
     const rootElement = document.getElementById('root')
     const rootTheme = rootElement.getAttribute('data-theme')
 
+    const [theme, setTheme] = useLocalStorage('theme' ? 'dark' : 'light')
+
     const toggleTheme = (e) => {
         e.preventDefault()
         if (rootTheme === 'light') {
             rootElement.setAttribute('data-theme', 'dark')
+            setTheme('dark')
         } else {
             rootElement.setAttribute('data-theme', 'light')
+            setTheme('light')
         }
       }
 
@@ -20,11 +25,13 @@ const Header = () => {
                 <Link to={'/'}>
                     <h2>My Blog</h2>
                 </Link>
-                <button onClick={(e) => toggleTheme(e)}></button>
+                <div className="toggleTheme">
+                    <i onClick={toggleTheme} className="fa-solid fa-toggle-on"></i>
+                </div>
             </div>
             <Nav />
         </header>
     )
 }
 
-export default Header
+export default Header 
